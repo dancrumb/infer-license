d604d166932fe52d5e92a2557d7b5aea11f8702a
Dan Rumney <dancrumb@gmail.com>
2017-01-18 14:28:22 -0600

    DRYing up my code

--- infer-license.js
+++ infer-license.js
@@ -1,7 +1,7 @@
 const BSD = /[Rr]edistribution\s*and\s*use\s*in\s*source\s*and\s*binary\s*forms,\s*with\s*or\s*without\s*modification,\s*are\s*permitted\s*provided\s*that\s*the\s*following\s*conditions\s*are\s*met/;
 const BSD_ORIGINAL = /All\s*advertising\s*materials\s*mentioning\s*features\s*or\s*use\s*of\s*this\s*software\s*must\s*display\s*the\s*following\s*acknowledgement/;
-const BSD_3 =/Neither\s*the\s*name\s*of\s*.*\s*nor\s*the\s*names\s*of\s*its\s*contributors\s*may\s*be\s*used\s*to\s*endorse\s*or\s*promote\s*products\s*derived\s*from\s*this\s*software\s*without\s*specific\s*prior\s*written\s*permission/;
-const MIT =/Permission\s*is\s*hereby\s*granted,\s*free\s*of\s*charge,\s*to\s*any\s*person\s*obtaining\s*a\s*copy\s*of\s*this\s*software\s*and\s*associated\s*documentation\s*files/;
+const BSD_3 = /Neither\s*the\s*name\s*of\s*.*\s*nor\s*the\s*names\s*of\s*its\s*contributors\s*may\s*be\s*used\s*to\s*endorse\s*or\s*promote\s*products\s*derived\s*from\s*this\s*software\s*without\s*specific\s*prior\s*written\s*permission/;
+const MIT = /Permission\s*is\s*hereby\s*granted,\s*free\s*of\s*charge,\s*to\s*any\s*person\s*obtaining\s*a\s*copy\s*of\s*this\s*software\s*and\s*associated\s*documentation\s*files/;
 const ISC = /Permission\s*to\s*use,\s*copy,\s*modify,\s*and\/or\s*distribute\s*this\s*software\s*for\s*any\s*purpose\s*with\s*or\s*without\s*fee\s*is\s*hereby\s*granted,\s*provided\s*that\s*the\s*above\s*copyright\s*notice\s*and\s*this\s*permission\s*notice\s*appear\s*in\s*all\s*copies./;
 const LGPL = /Everyone\s*is\s*permitted\s*to\s*copy\s*and\s*distribute\s*verbatim\s*copies\s*of\s*this\s*license\s*document,\s*but\s*changing\s*it\s*is\s*not\s*allowed./;
 const LGPL20 = /This\s*license,\s*the\s*Library\s*General\s*Public\s*License/;
@@ -16,11 +16,11 @@ const WTFPL = /FUCK/;
 
 
 function isBSD(text) {
-  if(BSD.test(text) && !APACHE.test(text)) {
-    if(BSD_ORIGINAL.test(text)) {
+  if (BSD.test(text) && !APACHE.test(text)) {
+    if (BSD_ORIGINAL.test(text)) {
       return 'BSD-4-Clause';
     } else if (BSD_3.test(text)) {
-      return'BSD-3-Clause';
+      return 'BSD-3-Clause';
     } else {
       return 'BSD-2-Clause';
     }
@@ -28,51 +28,49 @@ function isBSD(text) {
 }
 
 function isMIT(text) {
-  if(MIT.test(text)) {
+  if (MIT.test(text)) {
     return 'MIT';
   }
 }
 
 function isISC(text) {
-  if(ISC.test(text)) {
+  if (ISC.test(text)) {
     return 'ISC';
   }
 }
 
 function isEclipse(text) {
-  if(EPL10.test(text)) {
+  if (EPL10.test(text)) {
     return 'EPL-1.0';
   }
 }
 
-function identifyVersion(text, versions) {
-  return Object.keys(versions).find(function (spdx) {
-    return versions[spdx].test(text);
-  })
+function identifyVersion(text, baseLicense, versions) {
+  if (baseLicense.test(text)) {
+    return Object.keys(versions).find(function (spdx) {
+      return versions[spdx].test(text);
+    });
+  }
 }
 
 function isLGPL(text) {
-  if(LGPL.test(text)) {
-    return identifyVersion(text, {
-      'LGPL-2.0': LGPL20,
-      'LGPL-2.1': LGPL21,
-      'LGPL-3.0': LGPL30
-    });
-  }
+  return identifyVersion(text, LGPL, {
+    'LGPL-2.0': LGPL20,
+    'LGPL-2.1': LGPL21,
+    'LGPL-3.0': LGPL30
+  });
 }
 
 function isApache(text) {
-  if(APACHE.test(text)) {
-    return identifyVersion(text, {
-      'Apache-1.1': APACHE11,
-      'Apache-2.0': APACHE20,
-      'Apache-1.0': APACHE10
-    });
-  }
+  return identifyVersion(text, APACHE, {
+    'Apache-1.1': APACHE11,
+    'Apache-2.0': APACHE20,
+    'Apache-1.0': APACHE10
+  });
 }
 
 function isWTF(text) {
-  if(WTFPL.test(text)) {
+  if (WTFPL.test(text)) {
     return 'WTFPL';
   }
 }
@@ -86,11 +84,11 @@ module.exports = methods = {
   isApache: isApache,
   isEclipse: isEclipse,
   isWTF: isWTF,
-  inferLicense: function(text) {
+  inferLicense: function (text) {
     const matchingMethod = Object.keys(methods).find(function (methodName) {
       return /^is/.test(methodName) && methods[methodName](text);
     });
-    if(matchingMethod) {
+    if (matchingMethod) {
       return methods[matchingMethod](text);
     } else {
       return undefined;
